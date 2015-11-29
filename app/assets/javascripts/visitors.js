1ea0083892abc1957d290114102e59fcee25e518
dark419 <mr.ahlee@gmail.com>
2015-11-30 05:43:38 +1100

    first commit

---
+++ app/assets/javascripts/visitors.js
@@ -0,0 +1,25 @@
+var visitorCenter = angular.module('VisitorCenter', ['ngResource']);
+
+visitorCenter.factory("Visitor", function($resource) {
+  return $resource("visitors/:id", { id: '@id' }, {
+    index: { method: 'GET', isArray: true, responseType: 'json' },
+    update: { method: 'PUT', responseType: 'json' }
+  });
+})
+
+visitorCenter.controller("visitorController", function($scope, Visitor) {
+  $scope.visitors = Visitor.index()
+
+  $scope.addVisitor = function(){
+    visitor = Visitor.save($scope.newVisitor)
+
+    $scope.visitors.push(visitor)
+    $scope.newVisitor = {}
+  }
+
+  $scope.deleteVisitor = function() {
+    visitor = $scope.visitors[index]
+    Visitor.delete(visitor)
+    $scope.visitors.splice(index 1);
+  }
+})
